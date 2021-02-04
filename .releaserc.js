03fd12cce7ab003efb01b5efa8a911cba2d1651c
lmarqs <lucas.paula@wipro.com>
2021-02-04 09:06:18 -0300

    fix: deploy pipeline

--- .releaserc.js
+++ .releaserc.js
@@ -16,10 +16,10 @@ module.exports = {
       "prepareCmd": "lerna exec -- npm prune --production"
     }],
     ["@semantic-release/exec", {
-      "prepareCmd": "lerna exec -- ../../node_modules/.bin/npm-bundle"
+      "prepareCmd": "lerna exec -- node ../../node_modules/npm-bundled"
     }],
     ["@semantic-release/exec", {
-      "prepareCmd": "lerna exec -- ../../node_modules/.bin/shx cp *.tgz ../../release"
+      "prepareCmd": "lerna exec -- node ../../node_modules/.bin/shx cp *.tgz ../../release"
     }],
     ["@semantic-release/git", {
       "assets": ["CHANGELOG.md", "lerna.json", "package*.json", "packages/*/package*.json"],
